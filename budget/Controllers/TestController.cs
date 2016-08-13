7ab457e2ea73f9d89800477ccd3ebf566ff1d3f4
古元亮 <a4756830@gmail.com>
2016-08-13 11:13:34 +0800

    加入Facebook Api Share And Comment

---
+++ budget/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace budget.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+
+
+        public ActionResult Commont()
+        {
+            return this.View();
+        }
+
+        public ActionResult Share()
+        {
+            return this.View();
+        }
+
+
+
+    }
+}
