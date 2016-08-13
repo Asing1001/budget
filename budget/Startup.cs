3b02394afe1d3f40656a399d50c459b4e892836e
cs chen <cs chen>
2016-08-13 09:46:35 +0800

    first commit

---
+++ budget/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(budget.Startup))]
+namespace budget
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
