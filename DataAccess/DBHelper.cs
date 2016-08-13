9533db6e5ae35593dc91e59bb58faf13ccdbddda
a4756830 <a4756830@gmail.com>
2016-08-13 13:08:47 +0800

    data access, unit test

---
+++ DataAccess/DBHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class DBHelper
+    {        
+        //public static string connectionStr = "Data Source=southasia.database.windows.net;Initial Catalog=budgetDB;User ID=testuser;Password=MYPASSWORDHERE";
+        //public static string connectionStr = "Data Source=127.0.0.1;Initial Catalog=budget;Integrated Security=True";
+        public static string connectionStr = "Server=tcp:southasia.database.windows.net,1433;Initial Catalog=budgetDB;Persist Security Info=False;User ID=comma;Password=C@mma123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+       
+        public DBHelper()
+        {
+
+        }
+
+        public List<ENGINEER_BUDGET> Get_ENGINEER_BUDGET()
+        {
+            using(DataClassesDataContext db =new DataClassesDataContext(connectionStr))
+            {
+                return from q in db.ENGINEER_BUDGET select q;
+            }
+        }
+        
+        public List<ENGINEER_INFO> Get_ENGINEER_INFO()
+        {
+            using(DataClassesDataContext db =new DataClassesDataContext(connectionStr))
+            {
+                var query=from q in db.ENGINEER_INFO select q;
+            }
+        }
+
+        public List<ENGINEER_INFO> Get_NOT_CLOSE()
+        {
+            using(DataClassesDataContext db =new DataClassesDataContext(connectionStr))
+            {
+                var query=from q in db.NOT_CLOSE select q;
+            }
+        } 
+    }
+}
