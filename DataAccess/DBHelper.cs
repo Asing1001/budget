bdb31e117bc09a87f7ef3469d9349961a26bafde
a4756830 <a4756830@gmail.com>
2016-08-13 13:20:33 +0800

    add to solution file

--- DataAccess/DBHelper.cs
+++ DataAccess/DBHelper.cs
@@ -10,7 +10,7 @@ namespace DataAccess
     {        
         //public static string connectionStr = "Data Source=southasia.database.windows.net;Initial Catalog=budgetDB;User ID=testuser;Password=MYPASSWORDHERE";
         //public static string connectionStr = "Data Source=127.0.0.1;Initial Catalog=budget;Integrated Security=True";
-        public static string connectionStr = "Server=tcp:southasia.database.windows.net,1433;Initial Catalog=budgetDB;Persist Security Info=False;User ID=comma;Password=C@mma123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+        public static string connectionStr = "Server=tcp:southasia.database.windows.net,1433;Initial Catalog=budgetDB;Persist Security Info=False;User ID=comma;Password=C@mma123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
        
         public DBHelper()
         {
@@ -21,7 +21,7 @@ namespace DataAccess
         {
             using(DataClassesDataContext db =new DataClassesDataContext(connectionStr))
             {
-                return from q in db.ENGINEER_BUDGET select q;
+                return (from q in db.ENGINEER_BUDGET select q).ToList();
             }
         }
         
@@ -29,15 +29,15 @@ namespace DataAccess
         {
             using(DataClassesDataContext db =new DataClassesDataContext(connectionStr))
             {
-                var query=from q in db.ENGINEER_INFO select q;
+               return (from q in db.ENGINEER_INFO select q).ToList();
             }
         }
 
-        public List<ENGINEER_INFO> Get_NOT_CLOSE()
+        public List<NOT_CLOSE> Get_NOT_CLOSE()
         {
             using(DataClassesDataContext db =new DataClassesDataContext(connectionStr))
             {
-                var query=from q in db.NOT_CLOSE select q;
+                return (from q in db.NOT_CLOSE select q).ToList(); 
             }
         } 
     }
