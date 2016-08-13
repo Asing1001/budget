09a60b9fa68865cd275ba4115443e7a461e8a67f
AsinChen <shinningstar1001@gmail.com>
2016-08-13 17:21:11 +0800

    budget rc1

--- budget/Controllers/HomeController.cs
+++ budget/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace budget.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Analyze()
         {
             ViewBag.Message = "Your application description page.";
 
---
+++ budget/Scripts/app/controller/pieCtrl.js
@@ -0,0 +1,4 @@
+angular.module("ngApp").controller("pieCtrl", function ($scope) {
+  $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+  $scope.data = [300, 500, 100];
+});
